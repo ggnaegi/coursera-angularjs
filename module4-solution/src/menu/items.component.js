f7bb665964e518993d36a2d8b937314c13c15b9a
Guillaume Gnaegi <guillaume.gnaegi@ubit.ch>
2019-03-19 17:41:57 +0100

    component objects can be deleted. Component is dynamically defined in route.js

--- module4-solution/src/menu/items.component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('MenuApp')
-        .component('items', {
-            bindings: {
-                itemsList: '<'
-            }
-        })
-})();
