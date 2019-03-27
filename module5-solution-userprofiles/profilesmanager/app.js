47171a947d186924ff924f9f26ade51ec24a07be
Guillaume Gnaegi <guillaume.gnaegi@ubit.ch>
2019-03-27 23:47:03 +0100

    deploying to heroku

--- module5-solution-userprofiles/profilesmanager/app.js
+++ module5-solution-userprofiles/profilesmanager/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/userprofile', {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://mongodbsvc:Fo;hR^i.AGGatHC{KB4}@cluster0-0nxhw.azure.mongodb.net/userprofiles?retryWrites=true", {useNewUrlParser: true});
 
 const bodyParser = require('body-parser');
 const UserProfile  = require('./models/userprofile');
